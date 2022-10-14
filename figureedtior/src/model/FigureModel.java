07db1afb0b349379cf14b28733df5022e412a3a0
Sahil Jindal <sahiljindal@umass.edu>
2022-10-14 14:47:39 -0400

    Changes to FigureModel.java

--- figureedtior/src/model/FigureModel.java
+++ figureedtior/src/model/FigureModel.java
@@ -4,43 +4,77 @@ import javax.swing.ImageIcon;
 import java.beans.*;
 
 
-public class FigureModel
-{
+public class FigureModel {
+
+    // Private variable for image path
     private ImageIcon imagePath = null;
+    // Private variable for caption
     private String caption = null;
 
     private final PropertyChangeSupport propertyChangeSupport = new PropertyChangeSupport(this);
 
     public void FigureModel() {
+        
+        /**
+         * Constructor in case no image is given
+         */
     }
 
     public void FigureModel(ImageIcon image, String caption) {
+        
+        /**
+         * Sets the global variables for imagepath and caption and stores them
+         * Input: 
+         *      image - ImageIcon
+         *      caption - String
+         */
+
         this.imagePath = image;
         this.caption = caption;
     }
 
-    public void addPropertyChangeListener(PropertyChangeListener propertyChangeListener)
-    {
+    public void addPropertyChangeListener(PropertyChangeListener propertyChangeListener) {
+        
+        /**
+         * Adds Property Change Listener
+         * Input:
+         *      propertyChangeListener - PropertyChangeListener
+         */
+
         this.propertyChangeSupport.addPropertyChangeListener(propertyChangeListener);
     }
 
-    public void removePropertyChangeListener(PropertyChangeListener propertyChangeListener)
-    {
+    public void removePropertyChangeListener(PropertyChangeListener propertyChangeListener) {
+        
+        /**
+         * Removes the Property Change Listener
+         * Input:
+         *      propertyChangeListener - PropertyChangeListener
+         */
+
         this.propertyChangeSupport.removePropertyChangeListener(propertyChangeListener);
     }
 
     public ImageIcon getImage() {
+        
+        /**
+         * returns the stored image. If no image it returns null
+         */
+
 	    return this.imagePath;
     }
 
-    /**
-     * Sets the image to the given non-null ImageIcon.
-     *
-     * @param newImage The ImageIcon must be non-null
-     *
-     * @throws IllegalArgumentException if the ImageIcon is null
-     */
+    
     public void setImage(ImageIcon newImage) {
+        
+        /**
+         * Sets the image to the given non-null ImageIcon.
+         *
+         * @param newImage The ImageIcon must be non-null
+         *
+         * @throws IllegalArgumentException if the ImageIcon is null
+         */
+
         if (newImage == null) {
             throw new IllegalArgumentException("Your chosen image is null. Please reselect a non-null image!");
         }
@@ -49,17 +83,25 @@ public class FigureModel
     }
 
     public String getCaption() {
+        
+        /**
+         * returns the caption. If no caption it returns null
+         */
+
 	    return this.caption;
     }
 
-    /**
-     * Sets the caption to the given non-null and non-empty String.
-     *
-     * @param newCaption The String must be non-null and non-empty
-     *
-     * @throws IllegalArgumentException if the String is null or empty
-     */
+    
     public void setCaption(String newCaption) {
+        
+        /**
+         * Sets the caption to the given non-null and non-empty String.
+         *
+         * @param newCaption The String must be non-null and non-empty
+         *
+         * @throws IllegalArgumentException if the String is null or empty
+         */
+
         if (newCaption == null) {
             throw new IllegalArgumentException("Your chosen caption is null. Please rewrite a non-null caption!");
         }
@@ -68,14 +110,17 @@ public class FigureModel
         this.caption = newCaption;
     }
 
-    /**
-     * Returns true if this figure is complete, meaning its Image
-     * is non-null and its caption is non-null and non-empty, and
-     * false otherwise.
-     *
-     * @return True if this figure is complete and false otherwise
-     */
+    
     public boolean isComplete() {
+        
+        /**
+         * Returns true if this figure is complete, meaning its Image
+         * is non-null and its caption is non-null and non-empty, and
+         * false otherwise.
+         *
+         * @return True if this figure is complete and false otherwise
+         */
+
         return this.imagePath != null && (this.caption != null && this.caption.length() != 0);
     }
 }
